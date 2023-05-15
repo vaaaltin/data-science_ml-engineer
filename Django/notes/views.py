19a455b458456bcdb2076530a145c97f31b25e7e
Valter <vammartins@inf.ufpel.edu.br>
2023-05-15 00:28:10 -0300

    dynamic webpage and class based views

--- Django/notes/views.py
+++ Django/notes/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render
 from django.http import Http404
+from django.views.generic import DetailView, ListView
 
 from .models import Notes
 
+class NotesListView(ListView):
+    model = Notes
+    context_object_name = 'notes'
+    template_name = 'notes/notes_list.html'
 
-def list(request):
-    all_notes = Notes.objects.all()
-    return render(request, 'notes/notes_list.html', {'notes': all_notes})
-
-def detail(request, pk):
-    try:
-        note = Notes.objects.get(pk=pk)
-    except Notes.DoesNotExist:
-        raise Http404('Note does not exist')
-    return render(request, 'notes/notes_detail.html', {'note':note})
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = 'note'
