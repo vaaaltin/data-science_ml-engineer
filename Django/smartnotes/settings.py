9de5cb67adbe6e1e7db394a1a6373efca390a4b2
Valter <vammartins@inf.ufpel.edu.br>
2023-05-15 00:57:45 -0300

    style with bootstrap and dynamic generated links

--- Django/smartnotes/settings.py
+++ Django/smartnotes/settings.py
@@ -59,7 +59,9 @@ ROOT_URLCONF = 'smartnotes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'static/templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
