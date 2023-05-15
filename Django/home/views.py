b08afbb46bfa9b900966e6d066c6ae738b28a9e9
Valter <vammartins@inf.ufpel.edu.br>
2023-05-14 23:28:03 -0300

    users models django done

--- Django/home/views.py
+++ Django/home/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from datetime import datetime
 
 def home(request):
     return render(request, 'home/welcome.html', {'today': datetime.today()})
+
+
+@login_required(login_url='/admin')
+def authorized(request):
+    return render(request, 'home/authorized.html', {})
