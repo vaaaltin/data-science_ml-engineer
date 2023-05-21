04fffab38fff1faf5f693e5be59361d16c159a5f
Valter <vammartins@inf.ufpel.edu.br>
2023-05-20 23:57:31 -0300

    storing and displaying user specific data. title validation deleted

--- Django/notes/views.py
+++ Django/notes/views.py
@@ -1,7 +1,11 @@
+from typing import Any
+from django.http.response import HttpResponseRedirect
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.http import Http404
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
 from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import NotesForm
 from .models import Notes
@@ -16,15 +20,27 @@ class NotesUpdateView(UpdateView):
     success_url = '/smart/notes'
     form_class = NotesForm
 
-class NotesCreateView(CreateView):
+class NotesCreateView(LoginRequiredMixin, CreateView):
     model = Notes
     success_url = '/smart/notes'
     form_class = NotesForm
+    login_url = '/admin'
 
-class NotesListView(ListView):
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+
+        return HttpResponseRedirect(self.get_success_url())
+
+class NotesListView(LoginRequiredMixin, ListView):
     model = Notes
     context_object_name = 'notes'
     template_name = 'notes/notes_list.html'
+    login_url = '/admin'
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return self.request.user.notes.all()
 
 class NotesDetailView(DetailView):
     model = Notes
